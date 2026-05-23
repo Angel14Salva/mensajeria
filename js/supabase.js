de8ae80f8589af1a77ab6542637f7118c71f54c9
Angel14Salva <salvadorluis290@gmail.com>
2026-05-23 04:19:02 +0000

    configure supabase credentials

--- js/supabase.js
+++ js/supabase.js
@@ -3,7 +3,7 @@
 //  Reemplaza los valores con los de tu proyecto
 // =============================================
 
-const SUPABASE_URL = 'https://TU_PROJECT_ID.supabase.co';
-const SUPABASE_ANON_KEY = 'TU_ANON_KEY';
+const SUPABASE_URL = 'https://bbbwekrbeupuaymqxxgk.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_eyrvvqmYxJsYxg8ZGnDaaw_nj8CN7z2';
 
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
